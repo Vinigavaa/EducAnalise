04b341c2f4055a0cdda52c10a38d64f6fd7dad3e
vinicius.gava <vinigava14@gmail.com>
2025-11-10 17:25:20 -0300

    feat: iniciando routes materia

--- src/app/api/materias/[id]/route.ts
+++ src/app/api/materias/[id]/route.ts
@@ -1,6 +1,8 @@
 import { auth } from "@/lib/auth";
 import prisma from "@/lib/prisma";
+import { updateMateriaSchema } from "@/lib/validations/materia";
 import { NextRequest, NextResponse } from "next/server";
+import z from "zod";
 
 export async function GET(request: NextRequest, props: {params: Promise<{id: string}>}){
     const params = await props.params;
@@ -37,8 +39,57 @@ export async function PUT(request: NextRequest, props: {params: Promise<{id: str
     const params = await props.params;
 
     try{
-        
-    } catch(error){
+        const session = await auth();
 
+        if(!session || session.user?.id){
+            return NextResponse.json(
+                {error: "Não Autorizado"},
+                {status: 401}
+            )
+        }
+
+        const materiaExistente = await prisma.materia.findFirst({
+            where: {
+                id: params.id,
+                userId: session.user.id,
+            },
+        })
+
+        if(!materiaExistente){
+            return NextResponse.json(
+                {error: "Essa prova não existe"},
+                {status: 404}
+            )
+        }
+
+        const body = await request.json();
+
+        const validatedData = updateMateriaSchema.parse(body);
+
+        const materiaAtualizada = await prisma.materia.update({
+            where: {
+                id: params.id,
+            },
+            data: {
+                //Atualiza nome somente se existir em validatedData, Se não existir, data fica vazio ({}) e nada é alterado no banco
+                ...(validatedData.nome && {nome: validatedData.nome}),
+            }
+        });
+
+        return NextResponse.json(materiaAtualizada, {status: 200});
+    } catch(error){
+        if(error instanceof z.ZodError){
+            return NextResponse.json(
+                {error: "Dados Inválidos"},
+                {status: 400}
+            );
+        }
+
+        console.error("Erro ao atualizar Matéria!");
+
+        return NextResponse.json(
+            {error: "Erro ao atualizar Matéria!"},
+            {status: 500}
+        );
     }
 }
